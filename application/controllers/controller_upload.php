540fbe6f2cbbffaa7115d7a267a7610f88a0ef20
Denis Surin <pqdubpq@gmail.com>
2016-06-29 23:15:43 +0200

    Upload Page is Created

--- application/controllers/controller_upload.php
+++ application/controllers/controller_upload.php
@@ -6,4 +6,63 @@ class Controller_Upload extends Controller
     {
         $this->view->generate('upload_view.php', 'template_view.php');
     }
+    function action_transfer()
+    {
+        $dir = 'application/files/';
+
+        $max_file_size = $_POST['MAX_FILE_SIZE'];
+        
+        $file = $_FILES['uploadfile'];
+        $file_size = filesize($file['name']);
+        $file_mime = $_FILES['uploadfile']['type'];
+
+        if (!$error) {
+            if ($file['size'] == 0 && empty($file['name'])) {
+                $error = true;
+                $alert = 'danger';
+                $message = 'Файл не выбран!';
+            }
+        }
+
+        if (!$error) {
+            if ($file_size > $max_file_size) {
+                $error = true;
+                $alert = 'danger';
+                $message = 'Размер файла не должен превышать 2Мб!';
+            }
+        }
+        if (!$error) {
+            if ($file_mime != 'text/plain') {
+                $error = true;
+                $alert = 'danger';
+                $message = "Данный тип файла не разрешен для загрузки. Загрузите файл в формате .txt!";
+            }
+        }
+
+        $filename = mb_convert_encoding($file['name'], "Windows-1251", "UTF-8");
+
+        if (!fnmatch('*.txt', $filename)) {
+            $filename .= '.txt';
+        }
+        if (!$error) {
+            if (file_exists($dir . $filename)) {
+                $error = true;
+                $alert = 'danger';
+                $message = "Файл с таким именем существует!";
+            }
+        }
+        if (!$error) {
+            if (move_uploaded_file($file['tmp_name'], $dir . $filename)) {
+                $alert = 'success';
+                $message = 'Файл успешно загружен!';
+            } else {
+                $error = true;
+                $alert = 'danger';
+                $message = 'При загрузке файла произошла ошибка!';
+            }
+        }
+        
+        $data = ['error' => $error, 'alert' => $alert, 'message' => $message, 'file_size' => $file_size, 'filename' => $filename, 'max_file_size' => $max_file_size];
+        $this->view->generate('upload_view.php', 'template_view.php', $data);
+    }
 }
--- application/views/upload_view.php
+++ application/views/upload_view.php
@@ -1,1 +1,28 @@
-<h1>safbhk</h1>
+<?php
+$filename = $data['filename'];
+$error = $data['error'];
+$alert = $data['alert'];
+$message = $data['message'];
+
+echo $max_file_size = $data['max_file_size'];
+echo $file_size = $data['$file_size'];
+?>
+<h4>Загрузите свой файл в формате <i><b>.txt</b></i></h4>
+
+<p style="color: #777;"><i>Максимальный размер файла 2Мб</i></p>
+
+<form enctype="multipart/form-data" action="/upload/transfer" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="2097152"/>
+
+    <div class="form-group">
+        <input type="file" id="userFile" name="uploadfile">
+    </div>
+
+    <button type="submit" class="btn btn-primary">
+        <span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>&nbsp Upload file
+    </button>
+</form>
+
+<div class="alert alert-<?= $alert; ?>" role="alert">
+    <?= $message; ?>
+</div>
